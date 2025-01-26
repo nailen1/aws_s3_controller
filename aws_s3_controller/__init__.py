3f9796987ee53e8bd07fd72669f9e8db72e258fc
JuneYoung <nietsnie1@gmail.com>
2025-01-27 00:47:22 +0900

    refactor: improve module imports and exports

--- aws_s3_controller/__init__.py
+++ aws_s3_controller/__init__.py
@@ -4,43 +4,30 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.info("aws_s3_controller package initialized")
 
-from .s3_scanner import (
-    scan_files_in_bucket_by_regex,
-    scan_files_including_regex
-)
-
-from .s3_transfer import (
-    download_files_from_s3,
-    upload_files_to_s3,
-    relocate_files_between_buckets,
-    copy_files_including_regex_between_s3_buckets,
-    move_files_including_regex_between_s3_buckets
-)
-
-from .s3_dataframe_reader import (
-    open_df_in_bucket,
-    open_df_in_bucket_by_regex,
-    open_excel_in_bucket,
-    open_excel_in_bucket_by_regex
-)
-
-from .s3_structure import (
-    create_subfolder_in_bucket
-)
-
-from .s3_special_operations import (
-    locate_menu_datasets_from_s3_to_ec2web,
-    merge_timeseries_csv_files
-)
-
+from .aws_connector import S3, S3_WITHOUT_CREDENTIALS
+from .aws_consts import *
+from .s3_scanner import *
+from .s3_transfer import *
+from .s3_structure import *
+from .s3_dataframe_reader import *
+from .s3_special_operations import *
 from .alias import *
 
-import sys
-import inspect
-from . import S3Controller
-
-for name, obj in inspect.getmembers(S3Controller):
-    if inspect.isfunction(obj) or inspect.isclass(obj):
-        globals()[name] = obj
-
-__all__ = [name for name, obj in inspect.getmembers(S3Controller) if inspect.isfunction(obj) or inspect.isclass(obj)]
+__all__ = [
+    'S3',
+    'S3_WITHOUT_CREDENTIALS',
+    'scan_files_in_bucket_by_regex',
+    'download_files_from_s3',
+    'scan_files_including_regex',
+    'upload_files_to_s3',
+    'open_df_in_bucket',
+    'open_df_in_bucket_by_regex',
+    'open_excel_in_bucket',
+    'open_excel_in_bucket_by_regex',
+    'relocate_files_between_buckets',
+    'copy_files_including_regex_between_s3_buckets',
+    'move_files_including_regex_between_s3_buckets',
+    'create_subfolder_in_bucket',
+    'locate_menu_datasets_from_s3_to_ec2web',
+    'merge_timeseries_csv_files'
+]
